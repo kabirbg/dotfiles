c4cbbc6590564d36066a456a91d84c5a71988cd7
kabirbgupta <kabirg108@gmail.com>
2021-01-07 12:38:02 -0600

    doom config.el

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Kabir Gupta"
+      user-mail-address "kabirg108@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -52,3 +52,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+;;
+;; set DocView conversion refresh interval to 1 second
+(setq doc-view-conversion-refresh-interval 1)
