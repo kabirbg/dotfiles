72a1063d69f62cc17856b2a291fe757618288039
Kabir Gupta <kabirg108@gmail.com>
2022-02-01 09:48:36 -0500

    using crontab, and while loop in backup.sh

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if df /media/kabir/backup | egrep '9.%|100%' ; then
+while df /media/kabir/backup | egrep '9.%|100%' ; do
         echo disk full, removing oldest backup...
         find /media/kabir/backup -type f -printf '%T+ %p\n' | sort | awk -F ' ' '{print $2}' | head -n 1 | xargs rm -rvf
-fi
+done
 echo beginning backup @ $(date)
 tar -cvf "/media/kabir/backup/$(date +'%F-%H.%M.%S').tar" /bin /etc /home /root /sbin /usr /var
 echo finished backup @ $(date)
