6cf843c66b524785b3ca23820d918ddc72601c8b
Kabir Gupta <kabirg108@gmail.com>
2022-08-02 14:47:55 -0400

    fixed backup to use /mnt instead of /media for mount points

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 while true; do
-        while df /media/kabir/backup | egrep '9.%|100%'
+        while df /mnt/backup | egrep '9.%|100%'
         do
                 echo disk full, removing oldest backup...
-                find /media/kabir/backup -type f -printf '%T+ %p\n' | sort | awk -F ' ' '{print $2}' | head -n 1 | xargs rm -rvf
+                find /mnt/backup -type f -printf '%T+ %p\n' | sort | awk -F ' ' '{print $2}' | head -n 1 | xargs rm -rvf
         done
         echo beginning backup @ $(date)
-        tar cvzf "/media/kabir/backup/$(date +'%F-%H.%M.%S').tar" /bin /etc /home /opt /usr /var
+        tar cvzf "/mnt/backup/$(date +'%F-%H.%M.%S').tar" /bin /etc /home /opt /usr /var
         echo finished backup @ $(date)
         sleep 6h
 done
